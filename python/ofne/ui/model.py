a0fa963b3ba4c5e09e56548f99f47826d99ab6fa
Sol Kim <defreturnnone@gmail.com>
2026-01-08 17:23:33 +0900

    implement connection drawing

--- python/ofne/ui/model.py
+++ python/ofne/ui/model.py
@@ -1,16 +1,38 @@
-class OFnUINode(object):
-    def __init__(self, node):
-        super(OFnUINode, self).__init__()
-        self.__node = node
-        self.__pos_x = 0
-        self.__pos_y = 0
+from PySide6 import QtCore
 
 
-class OFnUIScene(object):
+class OFnUIScene(QtCore.QObject):
+    connected = QtCore.Signal(tuple)
+    disconnected = QtCore.Signal(tuple)
+
     def __init__(self, scene):
         super(OFnUIScene, self).__init__()
         self.__scene = scene
         self.__nodes = {}
+        self.__connections = set()
 
     def createNode(self, op_type):
-        print(op_type)
+        new_node = self.__scene.createNode(op_type)
+        if new_node:
+            self.__nodes[new_node.__hash__()] = new_node
+
+        return new_node
+
+    def connect(self, src, dst, index):
+        exh = None
+        inputs = dst.inputs()
+        if inputs[index] is not None:
+            exh = (inputs[index].__hash__(), dst.__hash__(), index)
+
+        res = dst.connect(src, index=index)
+        if res:
+            neh = (src.__hash__(), dst.__hash__(), index)
+            self.__connections.add(neh)
+
+            if exh:
+                self.__connections.remove(exh)
+                self.disconnected.emit(exh)
+
+            self.connected.emit(neh)
+
+        return res
