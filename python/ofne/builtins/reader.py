947bb1c2ea2cac47149dcf652776c351c9f0408e
Sol Kim <defreturnnone@gmail.com>
2026-01-20 23:54:37 +0900

    add builtin plugins

---
+++ python/ofne/builtins/reader.py
@@ -0,0 +1,32 @@
+from ofne import plugin
+import os
+import OpenImageIO as oiio
+
+
+class ReadImage(plugin.OFnOp):
+    def __init__(self):
+        super(ReadImage, self).__init__()
+
+    def params(self):
+        return {
+            "path": plugin.OFnParamStr("")
+        }
+
+    def needs(self):
+        return 0
+
+    def packetable(self):
+        return True
+
+    def operate(self, params, packetArray):
+        path = params.get("path")
+
+        if not path or os.path.isfile(path):
+            return plugin.OFnPacket()
+
+        try:
+            buf = oiio.ImageBuf(path)
+        except:
+            return plugin.OFnPacket()
+
+        return plugin.OFnPacket(data=buf.get_pixels(format=oiio.FLOAT))
