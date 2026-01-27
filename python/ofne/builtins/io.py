a12b2eb8a11ec0e29c3dad9f31f87fb29a4f9afa
Sol Kim <defreturnnone@gmail.com>
2026-01-27 22:51:33 +0900

    add ConstantImage node

--- python/ofne/builtins/io.py
+++ python/ofne/builtins/io.py
@@ -1,4 +1,5 @@
 from ofne import plugin
+import numpy as np
 import os
 import OpenImageIO as oiio
 
@@ -30,3 +31,42 @@ class ReadImage(plugin.OFnOp):
         buf = oiio.ImageBuf(path)
 
         return plugin.OFnPacket(data=buf.get_pixels(format=oiio.FLOAT))
+
+
+class ConstantImage(plugin.OFnOp):
+    def __init__(self):
+        super(ConstantImage, self).__init__()
+
+    def params(self):
+        return [
+            plugin.OFnParamInt("width", 512, min=2),
+            plugin.OFnParamInt("height", 512, min=2),
+            plugin.OFnParamFloat("R", 1.0),
+            plugin.OFnParamFloat("G", 1.0),
+            plugin.OFnParamFloat("B", 1.0),
+            plugin.OFnParamFloat("A", 1.0),
+        ]
+
+    def needs(self):
+        return 0
+
+    def packetable(self):
+        return True
+
+    def operate(self, params, packetArray):
+        return plugin.OFnPacket(
+            data=np.full(
+                (
+                    params.get("height"),
+                    params.get("width"),
+                    4
+                ),
+                [
+                    params.get("R"),
+                    params.get("G"),
+                    params.get("B"),
+                    params.get("A")
+                ],
+                dtype=np.float32
+            )
+        )
